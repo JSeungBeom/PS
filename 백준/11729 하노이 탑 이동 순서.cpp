f533186a8bbde4a82ccd842e429bc93859d413da
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-03-13 23:52:21 +0900

    baekjoon

---
+++ 백준/11729 하노이 탑 이동 순서.cpp
@@ -0,0 +1,42 @@
+/*
+세 개의 장대가 있고 첫 번째 장대에는 반경이 서로 다른 n개의 원판이 쌓여 있다. 
+각 원판은 반경이 큰 순서대로 쌓여있다. 
+이제 수도승들이 다음 규칙에 따라 첫 번째 장대에서 세 번째 장대로 옮기려 한다.
+
+한 번에 한 개의 원판만을 다른 탑으로 옮길 수 있다.
+쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
+이 작업을 수행하는데 필요한 이동 순서를 출력하는 프로그램을 작성하라. 
+단, 이동 횟수는 최소가 되어야 한다.
+
+아래 그림은 원판이 5개인 경우의 예시이다.
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int cnt = 0;
+
+void hanoi(int n, int from, int by, int to) {
+	if (n == 1)
+		cout << from << " " << to << endl;
+
+	else {
+		hanoi(n - 1, from, to, by);
+		cout << from << " " << to << endl;
+		hanoi(n - 1, by, from, to);
+	}
+}
+
+int main() {
+	int n;
+
+	cin >> n;
+
+	int k = pow(2, n) - 1;
+	cout << k << endl;
+
+	hanoi(n, 1, 2, 3);
+
+	return 0;
+}
