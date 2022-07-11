5505f16b5a9bdfce9432c9f0a556c8fc0deb12c9
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-07-11 21:26:59 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < i; k++) {
+			cout << " ";
+		}
+
+		for (int j = n - i; j > 0; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+
+	}
+}
