0ee04d049dca8897367395f71458498971ef0428
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-07-11 21:56:16 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n - 1; i++) {
+		for(int j = i; j < (2 * n - 1) / 2; j++){
+			cout << " ";
+		}
+		for (int j = 0; j < (2 * (i + 1)) - 1; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	for (int i = n; i > 0; i--) {
+		for (int j = n; j > i; j--) {
+			cout << " ";
+		}
+		for (int j = 0; j < 2 * i - 1; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
