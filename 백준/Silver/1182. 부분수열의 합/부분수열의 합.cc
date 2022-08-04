66b6caea7f597fd661833003efe37642cc3a3f44
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-08-04 19:27:20 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, s;
+int arr[30];
+int cnt = 0;
+
+void func(int cur, int total) {
+	if (cur == n) {
+		if (total == s) {
+			cnt++;
+		}
+		return;
+	}
+
+	func(cur + 1, total);
+	func(cur + 1, total + arr[cur]);
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	func(0, 0);
+	if (s == 0) {
+		cnt--;
+	}
+
+	cout << cnt;
+}
