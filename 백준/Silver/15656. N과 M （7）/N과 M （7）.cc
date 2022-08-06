17adcbf3d1c7f05f7f26945e2de6acded295a6d6
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-08-06 22:23:00 +0900

    [Silver III] Title: N과 M (7), Time: 476 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int temp[10];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		arr[k] = temp[i];
+		func(k + 1);
+	}
+}
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp[i];
+	}
+
+	sort(temp, temp + n);
+	func(0);
+}
