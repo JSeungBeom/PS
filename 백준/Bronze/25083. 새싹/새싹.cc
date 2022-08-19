ff2e950628cc3f35c350ae741f37aef7a3517790
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-08-19 22:36:34 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+
+int arr[100002];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+}
