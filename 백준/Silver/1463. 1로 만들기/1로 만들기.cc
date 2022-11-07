f801060ebd4f4d7c99e4a71954544dec6b611e06
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-11-07 23:40:22 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1000005];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	arr[1] = 0;
+	int n;
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + 1;
+
+		if (i % 3 == 0) arr[i] = min(arr[i], arr[i / 3] + 1);
+		if (i % 2 == 0) arr[i] = min(arr[i], arr[i / 2] + 1);
+	}
+
+
+
+	cout << arr[n];
+}
