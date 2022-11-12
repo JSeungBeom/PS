b7ca7ba82c3ef78eb4f5510b54ab3072236db157
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-11-12 21:06:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n; 
+int d[1005]; // d[i] : 2 x i 직사각형을 채우는 방법의 수
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+		
+	cin >> n;
+	d[1] = 1;
+	d[2] = 3;
+
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + d[i - 2] + d[i - 2]) % 10007;
+	}
+
+	cout << d[n];
+}
