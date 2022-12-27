66a23ea12dc73c4d7617f7b95439c7709b958158
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-12-27 18:03:32 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.cc
@@ -1,22 +1,32 @@
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n;
-int a[1010], d[1010];
+int arr[1003];
+int d[1003];
+
 
 int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-  cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> a[i];
-    d[i] = a[i];
-  }
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < i; ++j)
-      if (a[j] < a[i]) d[i] = max(d[i], d[j] + a[i]);
-  
-  cout << *max_element(d, d + n);
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+		d[i] = arr[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			if (arr[i] > arr[j]) {
+				d[i] = max(d[i],d[j] + arr[i]);
+			}
+		}
+	}
+
+	cout << *max_element(d + 1, d + 1 + n);
 }
+
+// 1 101 103 153 213 
