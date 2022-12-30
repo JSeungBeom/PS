8a5666cbfaa5eedf651641dc6a3ae7b97d071cef
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2022-12-30 23:16:24 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+
+using namespace std;
+
+int n;
+int t[20];
+int p[20];
+int d[20];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> t[i] >> p[i];
+	}
+
+	for (int i = n; i >= 1; i--) {
+		if(i + t[i] <= n + 1)
+		{
+			d[i] = max(d[i + t[i]] + p[i], d[i + 1]);
+		}
+		else {
+			d[i] = d[i + 1];
+		}
+	}
+
+	cout << *max_element(d + 1, d + 1 + n);
+
+}
