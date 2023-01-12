0f53270cef32b0e3e2539cddbfed6a0eba0a0fdb
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-01-12 19:30:26 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "CY";
+	}
+	else {
+		cout << "SK";
+	}
+}
