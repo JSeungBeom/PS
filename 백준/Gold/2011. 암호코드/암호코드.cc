11fde7f508042a59c05dd50d76deb2d6fb7577da
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-01-25 22:56:02 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int a[5010], d[5010];
+int mod = 1000000;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+    n = s.size();
+    for (int i = 1; i <= n; ++i) a[i] = s[i - 1] - '0';
+    d[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        if (a[i] > 0) d[i] = (d[i] + d[i - 1]) % mod;
+        int x = a[i - 1] * 10 + a[i];
+        if (x >= 10 && x <= 26) d[i] = (d[i] + d[i - 2]) % mod;
+    }
+    cout << d[n];
+}
