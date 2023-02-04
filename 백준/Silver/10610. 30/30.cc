2241dd24a14e028de38b041aedc64611c5ee1f38
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-02-04 23:41:14 +0900

    [Silver IV] Title: 30, Time: 8 ms, Memory: 3052 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string N;
+vector<int> nums;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+  for (char c : N)
+    nums.push_back(c - '0');
+
+  sort(nums.begin(), nums.end(), greater<int>());
+  int sum = accumulate(nums.begin(), nums.end(), 0);
+  if (sum % 3 == 0 && nums.back() == 0)
+    for (int n : nums) cout << n;
+  else
+    cout << -1;
+}
