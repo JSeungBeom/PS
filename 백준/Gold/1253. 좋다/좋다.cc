0dad0f760f3d9157a2dd2ecc42e28a356263b301
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-02-23 22:54:05 +0900

    [Gold IV] Title: 좋다, Time: 112 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cnt, n;
+vector<int> a;
+
+void solve(int i) {
+  for(int j = 0; j < n; j++) {
+    if(j == i) continue;
+    int x = a[i] - a[j];
+    int idx = lower_bound(a.begin(), a.end(), x) - a.begin();
+    while(idx < n && a[idx] == x) {
+      if(idx != i && idx != j) { cnt++; return; }
+      idx++;
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    int x; cin >> x;
+    a.push_back(x);
+  }
+
+  sort(a.begin(), a.end());
+
+  for(int i = 0; i < n; i++) solve(i);
+  cout << cnt;
+}
