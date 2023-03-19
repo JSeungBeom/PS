6a14ba1331895f15fab16dede365ba9d8c0c80e7
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-03-19 22:16:12 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+int t, n;
+string name, type;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> t;
+
+	while (t--) {
+		unordered_map<string, int> clothes;
+		int ans = 1;
+		cin >> n;
+		while (n--) {
+			cin >> name >> type;
+			clothes[type]++;
+		}
+
+		for (auto e : clothes) {
+			ans *= e.second + 1;
+		}
+		cout << ans - 1 << "\n";
+	}
+
+
+}
+
