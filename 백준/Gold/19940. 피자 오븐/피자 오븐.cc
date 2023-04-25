8df78bd830fa8e04b69339ea38929d39187557fb
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-04-25 12:26:08 +0900

    [Gold V] Title: 피자 오븐, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/19940. 피자 오븐/피자 오븐.cc
+++ 백준/Gold/19940. 피자 오븐/피자 오븐.cc
@@ -2,52 +2,7 @@
 
 using namespace std;
 
-int dx[5] = { 60, 10, -10, 1, -1 };
-int dist[65];
-queue<int> q;
-string dp[65];
 int t, n;
-
-bool cmp(string a, string b) {
-	int len_a = 0, len_b = 0;
-
-	for (int i = 0; i < 5; i++) {
-		len_a += a[i] - '0';
-		len_b += b[i] - '0';
-	}
-
-	if (len_a != len_b) return len_a < len_b;
-	return a < b;
-}
-
-void BFS() {
-	for (int i = 0; i <= 60; i++) {
-		dp[i] = "99999";
-	}
-
-	dp[0] = "00000";
-	q.push(0);
-	dist[0] = 0;
-
-	while (!q.empty()) {
-		int cur = q.front(); q.pop();
-
-		for (int dir = 0; dir < 5; dir++) {
-			int nxt = cur + dx[dir];
-
-			if (nxt < 0 || nxt > 60) continue;
-
-			string tmp = dp[cur];
-			tmp[dir]++;
-
-			if (cmp(tmp, dp[nxt])) {
-				dp[nxt] = tmp;
-				q.push(nxt);
-			}
-		}
-	}
-
-}
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -57,18 +12,39 @@ int main(void) {
 
 	while (t--) {
 		cin >> n;
-		int ans[5] = { 0, };
-		ans[0] += n / 60;
-		n %= 60;
 
-		BFS();
-		for (int i = 0; i < 5; i++ ) {
-			ans[i] += dp[n][i] - '0';
+		int ans[5] = { 0, };
+		ans[0] += n / 60; // 전처리, ADDH 횟수 추가
+		n %= 60; // 0 ~ 59분 
+
+		// 35분 넘어가면, 60분에서 빼는게 최소 횟수
+		if (n > 35) {
+			ans[0]++; // ADDH 
+			// 일의 자리가 5 이상이면, 10분 단위에서 빼는 게 최소 횟수
+			if (n % 10 >= 5) {
+				ans[2] += 6 - (n / 10 + 1); // MINT
+				ans[4] += 10 - (n % 10); // MIN0
+			}
+			else {
+				ans[2] += 6 - (n / 10); // ADDT
+				ans[3] += n % 10; // ADD0
+			}
+		}
+		else { // 35분 이하라면, ADDT하는게 최소 횟수
+			if (n % 10 > 5) { // 일의 자리가 5 넘어가면, 10분 단위에서 빼는 게 최소 횟수
+				ans[1] += n / 10 + 1;
+				ans[4] += 10 - ( n % 10);
+			}
+			else {
+				ans[1] += n / 10;
+				ans[3] += n % 10;
+			}
 		}
 
 		for (int i = 0; i < 5; i++) {
 			cout << ans[i] << " ";
 		}
+
 		cout << "\n";
 	}
 }
