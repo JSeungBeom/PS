bbeca11679c76e938dcdff5cdb1d32497d8d1c97
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-05-10 12:49:55 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 3356 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -2,67 +2,63 @@
 
 using namespace std;
 
-int vertex1, vertex2, weight;
-int V, E;
-// 가중치, 정점1, 정점2를 담는 배열
-tuple<int, int, int> edges[100003];
+int v, e;
+tuple<int, int, int> adj[100003];
+vector<int> p(10005, 0);
 
-// Union Find를 진행할 정점 배열
-vector<int> parents(10003, 0);
-
-int getParent(int x) {
-	if (parents[x] == x) return x;
-	return parents[x] = getParent(parents[x]);
+int find(int x) {
+	if (p[x] == x) return x;
+	return p[x] = find(p[x]);
 }
 
 void unionParent(int u, int v) {
-	u = getParent(u);
-	v = getParent(v);
-	if (u < v) parents[u] = v;
-	else parents[v] = u;
+	u = find(u);
+	v = find(v);
+
+	if (u < v) p[u] = v;
+	else p[v] = u;
 }
 
-bool is_different_group(int u, int v) {
-	u = getParent(u);
-	v = getParent(v);
+bool is_diff_group(int u, int v) {
+	u = find(u);
+	v = find(v);
 
 	if (u == v) return 0;
 	return 1;
 }
 
 int main(void) {
-	ios::sync_with_stdio(0); cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-	cin >> V >> E;
+	cin >> v >> e;
 
-	int ans = 0;
+	for (int i = 1; i <= v; i++) {
+		p[i] = i;
+	}
+
+	int a, b, weight;
+	for(int i = 0; i < e; i++) {
+		cin >> a >> b >> weight;
+		adj[i] = { weight, a, b };
+	}
+
+	sort(adj, adj + e);
 	int cnt = 0;
-
-	for (int i = 1; i <= V; i++) {
-		parents[i] = i;
-	}
-
-	for (int i = 0; i < E; i++) {
-		cin >> vertex1 >> vertex2 >> weight;
-		edges[i] = { weight, vertex1, vertex2 };
-	}
-
-	// 간선의 가중치, 정점1, 정점2 순으로 오름차순으로 정렬
-	sort(edges, edges + E); // sort 함수의 특성에 따라, O(ElogE) time
-
-	for (int i = 0; i < E; i++) {
-		int a, b, cost;
-		tie(cost, a, b) = edges[i]; // 가중치, 정점 a, 정점 b 꺼내기
-		// 정점 a, 정점 b가 다른 그룹이라면(즉, 사이클을 이루지 않는다면)
-		if (is_different_group(a, b)) { 
-			unionParent(a, b); // 최소 신장 트리에 간선 추가
-			ans += cost; // 가중치 더하기
-			cnt++; // 간선 개수 세기
+	long long ans = 0;
+		
+	for (int i = 0; i < e; i++) {
+		tie(weight, a, b) = adj[i];
+		if (is_diff_group(a, b)) {
+			unionParent(a, b);
+			ans += weight;
+			cnt++;
 		}
 
-		// 간선 개수가 V - 1이라면, 최소 신장 트리가 완성이므로 빠져나가기
-		if (cnt == V - 1) break;
+		if (cnt == v - 1) break;
 	}
 
 	cout << ans;
+
+
 }
