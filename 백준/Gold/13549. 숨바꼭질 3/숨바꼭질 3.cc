b164a2d92b586fc82f69129667ed99ea087c3c63
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-07-25 16:33:26 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,38 +1,55 @@
 #include <bits/stdc++.h>
+#define X first
+#define Y second
+
 using namespace std;
 
-int dist[200002];
+int n, k;
+bool vis[100003];
+queue<pair<int, int>> q;
 
+void BFS() {
+	if (n == k) {
+		cout << 0;
+		return;
+	}
+
+	q.push({n, 0});
+	vis[n] = 1;
+
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+
+		if (cur.X == k) {
+			cout << cur.Y; return;
+		}
+
+		for (int nxt : {cur.X * 2, cur.X - 1, cur.X + 1}) {
+
+			if (nxt == cur.X * 2 && nxt <= 100000
+				&& !vis[nxt]) {
+				vis[nxt] = 1;
+				q.push({nxt, cur.Y});
+			}
+			if (nxt == cur.X - 1 && nxt >= 0 && !vis[nxt]) {
+				vis[nxt] = 1;
+				q.push({nxt, cur.Y + 1});
+			}
+
+			if (nxt == cur.X + 1 && nxt <= 100000 && !vis[nxt]) {
+				vis[nxt] = 1;
+				q.push({ nxt, cur.Y + 1});
+			}
+		}
+	}
+}
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, k;
 	cin >> n >> k;
 
-	fill(dist, dist + 200000, -1);
+	BFS();
 
-	deque <int> Q;
-	Q.push_back(n);
-	dist[n] = 0;
-
-	// 범위 내의 모든 거리를 계산
-	while (!Q.empty()) {
-		int cur = Q.front(); Q.pop_front();
-		// 순간이동 하는 인덱스의 거리를 동일하게 맞춘다
-		if (cur * 2 < 200000 && dist[cur * 2] == -1) {
-			dist[cur * 2] = dist[cur];
-			Q.push_front(cur * 2);
-		}
-
-		// 그냥 이동하는 경우
-		for (int v : {cur + 1, cur - 1}) {
-			if (v < 0 || v >= 200000 || dist[v] != -1) continue;
-
-			dist[v] = dist[cur] + 1;
-			Q.push_back(v);
-		}
-	}
-	cout << dist[k];
 }
