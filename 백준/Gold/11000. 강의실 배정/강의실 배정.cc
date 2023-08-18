c19375a5f573e2ac06dd159d555d9dc9919102b7
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2023-08-18 16:35:40 +0900

    [Gold V] Title: 강의실 배정, Time: 60 ms, Memory: 4484 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+int n;
+
+struct node {
+    int s,t;
+};
+
+node arr[200001];
+
+bool cmp(node a, node b) {
+    if(a.s==b.s) {
+        return a.t<b.t;
+    }
+    return a.s<b.s;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    for(int i=0 ; i<n ; i++) {
+        cin >> arr[i].s >> arr[i].t;
+    }
+    sort(arr, arr+n, cmp);
+
+    priority_queue<int, vector<int>, greater<int>> q;
+    q.push(arr[0].t);
+    for(int i=1 ; i<n ; i++) {
+        int s = arr[i].s, t = arr[i].t;
+        int now = q.top();
+        if(now>s) {
+            q.push(t);
+        } else {
+            q.pop();
+            q.push(t);
+        }
+    }
+    cout << q.size();
+    return 0;
+}
