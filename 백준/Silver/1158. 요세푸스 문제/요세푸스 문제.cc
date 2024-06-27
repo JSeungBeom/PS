d7ccc64eb9988806f321b6a15a65787480f034f0
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-06-27 21:27:21 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 92 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,51 +1,38 @@
-#include <bits/stdc++.h>	
+#include <bits/stdc++.h>
 
 using namespace std;
+int n, k;
+list<int> l;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, k;
-
 	cin >> n >> k;
 
-	list<int> num;
-	int arr[5005] = {};
-
 	for (int i = 1; i <= n; i++) {
-		num.push_back(i);
+		l.push_back(i);
 	}
 
-	auto p = num.begin();
-
-	int idx = 0;
-	while (!num.empty()) {
-		for (int i = 0; i < k - 1; i++) {
-			if (p != num.end()) {
-				p++;
-			}
-			else {
-				p = num.begin();
-				p++;
-			}
-		}
-
-		if (p == num.end()) {
-			p = num.begin();
-		}
-		arr[idx++] = *p;
-		p = num.erase(p);
-	}
-
+	auto it = l.begin();
 	cout << "<";
-
-	for (int i = 0; i < n; i++) {
-		cout << arr[i];
-		if (i != n - 1) {
-			cout << ", ";
+	while (!l.empty()) {
+		for (int i = 0; i < k - 1; i++) {
+			if (it == l.end()) {
+				it = l.begin(); it++;
+			}
+			else
+				it++;
 		}
+		if (it == l.end())
+			it = l.begin();
+		if (l.size() > 1)
+			cout << *it << ", ";
+		else
+			cout << *it;
+		it = l.erase(it);
 	}
-
 	cout << ">";
+
+
 }
