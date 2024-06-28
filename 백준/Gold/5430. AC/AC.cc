100fec8c98aacd2339a2dcc81aa6cd0c945673c8
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-06-28 19:15:36 +0900

    [Gold V] Title: AC, Time: 36 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -2,73 +2,77 @@
 
 using namespace std;
 
-int main(void) {
-	int t;
-	cin >> t;
+int T;
+string p;
+int n;
 
-	while (t--) {
-		string func;
-		cin >> func;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-		deque<int> arr;
-		bool flag = true;
+	cin >> T;
 
-		int n;
+	while (T--) {
+		deque<int> dq;
+		cin >> p;
 		cin >> n;
 
 		string s;
 		cin >> s;
+		bool flag = 1;
 
 		int cur = 0;
+
 		for (int i = 1; i < s.length() - 1; i++) {
 			if (s[i] == ',') {
-				arr.push_back(cur);
+				dq.push_back(cur);
 				cur = 0;
 			}
 			else {
 				cur = 10 * cur + (s[i] - '0');
 			}
 		}
-
 		if (cur != 0) {
-			arr.push_back(cur);
+			dq.push_back(cur);
 		}
-
+	
 		int r = 0;
-		for (int i = 0; i < func.length(); i++) {
-			if (func[i] == 'R') {
+		for (int i = 0; i < p.length(); i++) {
+			if (p[i] == 'R') {
 				r = 1 - r;
 			}
 			else {
-				if (arr.empty()) {
-					flag = false;
-					cout << "error\n";
+				if (dq.empty()) {
+					flag = 0;
 					break;
 				}
 				else {
 					if (r) {
-						arr.pop_back();
+						dq.pop_back();
 					}
 					else {
-						arr.pop_front();
+						dq.pop_front();
 					}
 				}
 			}
 		}
 
-		if (flag) {
-			cout << "[";
-			if (r) {
-				reverse(arr.begin(), arr.end());
+		if (!flag)
+			cout << "error\n";
+		else {
+			if (dq.empty()) {
+				cout << "[]\n";
 			}
-			for (int i = 0; i < arr.size(); i++) {
-				
-				cout << arr[i];
-				if (i != arr.size() - 1) {
-					cout << ",";
+			else {
+				if (r)
+					reverse(dq.begin(), dq.end());
+				cout << "[";
+				for (int i = 0; i < dq.size() - 1; i++) {
+					cout << dq[i] << ',';
 				}
+				cout << dq.back() << "]\n";
 			}
-			cout << "]\n";
 		}
 	}
+
 }
