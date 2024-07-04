75be56250272e8ceeb3707bbc32b62b5d299413d
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-04 17:15:44 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -4,30 +4,39 @@ using namespace std;
 
 int n, m;
 int arr[10];
-bool isused[10];
+int ans[10];
+bool vis[10];
 
-void func(int k) {
-	if (k == m) {
+void back_tracking(int len) {
+	if (len == m) {
 		for (int i = 0; i < m; i++) {
-			cout << arr[i] << " ";
+			cout << ans[i] << ' ';
 		}
-		cout << "\n";
+		cout << '\n';
+
 		return;
 	}
 
 	for (int i = 1; i <= n; i++) {
-		if (!isused[i]) {
-			arr[k] = i;
-			isused[i] = 1;
-			func(k + 1);
-			isused[i] = 0;
-		}
+		if (vis[i])
+			continue;
+
+		vis[i] = 1;
+		ans[len] = arr[i];
+		back_tracking(len + 1);
+		vis[i] = 0;
 	}
 }
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n >> m;
-	func(0);
+
+	for (int i = 1; i <= n; i++)
+		arr[i] = i;
+
+	back_tracking(0);
+
 }
