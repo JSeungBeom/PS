12083937201feba57cac18817f8eaff0e8049dd4
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-04 17:38:19 +0900

    [Gold IV] Title: N-Queen, Time: 1676 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,11 +3,11 @@
 using namespace std;
 
 int n;
-bool isused1[30];
-bool isused2[30];
-bool isused3[30];
+int cnt;
 
-int cnt = 0;
+bool isused1[20];
+bool isused2[50];
+bool isused3[50];
 
 void func(int cur) {
 	if (cur == n) {
@@ -16,23 +16,27 @@ void func(int cur) {
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (!isused1[i] && !isused2[cur + i] && !isused3[cur - i + n - 1]) {
-			isused1[i] = 1;
-			isused2[cur + i] = 1;
-			isused3[cur - i + n - 1] = 1;
-			func(cur + 1);
-			isused1[i] = 0;
-			isused2[cur + i] = 0;
-			isused3[cur - i + n - 1] = 0;
+		if (isused1[i]
+			|| isused2[cur + i]
+			|| isused3[cur - i + n - 1])
+			continue;
 
-		}
+		isused1[i] = 1;
+		isused2[cur + i] = 1;
+		isused3[cur - i + n - 1] = 1;
+		func(cur + 1);
+		isused1[i] = 0;
+		isused2[cur + i] = 0;
+		isused3[cur - i + n - 1] = 0;
 	}
 }
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n;
+
 	func(0);
 
 	cout << cnt;
