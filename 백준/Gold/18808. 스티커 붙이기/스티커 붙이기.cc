41e6a56d84d561c50083768ac8d31c87025a395c
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-05 22:06:17 +0900

    [Gold III] Title: 스티커 붙이기, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
@@ -1,84 +1,90 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m, k;
-int note[50][50];
-int board[12][12];
-int cnt = 0;
-int r, c;
+int n, m, k, r, c;
 
-bool pastable(int x, int y) {
+int board[50][50];
+int sticker[15][15];
+
+bool chk(int x, int y) {
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			if (note[x + i][y + j] == 1 && board[i][j] == 1)
-				return false;
+			if (board[x + i][y + j] == 1 && sticker[i][j] == 1)
+				return 0;
 		}
 	}
 
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			if (board[i][j] == 1) {
-				note[x + i][y + j] = 1;
-			}
+			if (sticker[i][j] == 1)
+				board[x + i][y + j] = 1;
 		}
 	}
 
-	return true;
+	return 1;
 }
 
-void spin() {
-	int tmp[12][12];
+void rotate() {
+	int tmp[15][15];
+
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
-			tmp[i][j] = board[i][j];
+			tmp[i][j] = sticker[i][j];
 		}
 	}
+
 	for (int i = 0; i < c; i++) {
 		for (int j = 0; j < r; j++) {
-			board[i][j] = tmp[r - 1 - j][i];
+			sticker[i][j] = tmp[r - 1 - j][i];
 		}
 	}
-	
+
 	swap(r, c);
 }
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+
 	cin >> n >> m >> k;
-	for (int a = 0; a < k; a++) {
+
+
+	while (k--) {
 		cin >> r >> c;
 
 		for (int i = 0; i < r; i++) {
 			for (int j = 0; j < c; j++) {
-				cin >> board[i][j];
+				cin >> sticker[i][j];
 			}
 		}
 
 		for (int rot = 0; rot < 4; rot++) {
-			bool flag = false;
+			bool is_paste = 0;
 			for (int x = 0; x <= n - r; x++) {
-				if (flag) break;
+				if (is_paste) break;
 				for (int y = 0; y <= m - c; y++) {
-					if (pastable(x, y)) {
-						flag = true;
+					if (chk(x, y)) {
+						is_paste = true;
 						break;
 					}
 				}
 			}
 
-			if (flag)
-				break;
-			spin();
+			if (is_paste) break;
+			rotate();
 		}
+
 	}
 
+	int cnt = 0;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cnt += note[i][j] == 1;
+			cnt += board[i][j];
 		}
 	}
 
 	cout << cnt;
+
 }
