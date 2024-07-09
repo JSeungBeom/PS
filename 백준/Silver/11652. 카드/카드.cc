0f521b0113cf9da65ca753fdeafea041004177da
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-09 18:36:45 +0900

    [Silver IV] Title: 카드, Time: 28 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.cc
+++ 백준/Silver/11652. 카드/카드.cc
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-int cnt = 0;
-long long maxval = -(1ll << 62) - 1;
-int maxcnt = 0;
+using namespace std;
+using ll = long long;
+
 int n;
-long long arr[100002];
+ll arr[100005];
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
+	
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+
+	for (int i = 0; i < n; i++)
 		cin >> arr[i];
-	}
 
 	sort(arr, arr + n);
 
-	for (int i = 0; i < n; i++) {
-		if (i == 0 || arr[i] == arr[i - 1]) cnt++; 
-		else{
-			if (maxcnt < cnt) {
-				maxcnt = cnt;
-				maxval = arr[i - 1];
+	ll mxval = (1ll << 62) - 1;
+	ll mxcnt = 0;
+	int cnt = 1;
+
+	for (int i = 1; i < n; i++) {
+		if (arr[i] != arr[i - 1]) {
+			if (cnt > mxcnt) {
+				mxcnt = cnt;
+				mxval = arr[i - 1];
 			}
+
 			cnt = 1;
 		}
+		else {
+			cnt++;
+		}
 	}
-	if (maxcnt < cnt) maxval = arr[n - 1];
 
-	cout << maxval;
-	
+	if (cnt > mxcnt) mxval = arr[n - 1];
+	cout << mxval;
 }
