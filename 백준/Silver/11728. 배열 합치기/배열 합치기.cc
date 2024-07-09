2b1fa84a51b1a8810e21e2ccb0fceece67591c5e
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-09 15:49:22 +0900

    [Silver V] Title: 배열 합치기, Time: 508 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,40 +1,44 @@
 #include <bits/stdc++.h>
 
-int n, m;
-int arr1[1000002];
-int arr2[1000002];
-int arr[2000002];
-
 using namespace std;
+
+int a[1000003];
+int b[1000003];
+int c[2000005];
+int n, m;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> arr1[i];
-	for (int i = 0; i < m; i++) cin >> arr2[i];
 
-	int idx1 = 0;
-	int idx2 = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < m; i++) {
+		cin >> b[i];
+	}
+
+	int aidx = 0, bidx = 0;
 
 	for (int i = 0; i < n + m; i++) {
-		if (idx1 >= n) {
-			arr[i] = arr2[idx2++];
+		if (aidx >= n) {
+			c[i] = b[bidx++];
 		}
-		else if (idx2 >= m) {
-			arr[i] = arr1[idx1++];
+		else if (bidx >= m) {
+			c[i] = a[aidx++];
 		}
-		else if (arr1[idx1] < arr2[idx2])
-		{
-			arr[i] = arr1[idx1++];
+		else if (a[aidx] >= b[bidx]) {
+			c[i] = b[bidx++];
 		}
-		else {
-			arr[i] = arr2[idx2++];
+		else if (a[aidx] < b[bidx]) {
+			c[i] = a[aidx++];
 		}
 	}
 
 	for (int i = 0; i < n + m; i++) {
-		cout << arr[i] << " ";
+		cout << c[i] << ' ';
 	}
 }
