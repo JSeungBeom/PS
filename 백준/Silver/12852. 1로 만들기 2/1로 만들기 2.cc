e689633cad1745ddac208c7b0cac0249659caaf8
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-10 19:39:27 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -3,44 +3,40 @@
 using namespace std;
 
 int n;
-int d[1000002];
-int pre[1000002];
+int dp[1000006];
+int pre[1000006];
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n;
+	
+	dp[1] = 0;
 
-	d[1] = 0;
-	d[2] = 1;
-	d[3] = 1;
-
-	pre[1] = 0;
-	pre[2] = 1;
-	pre[3] = 1;
-
-	for (int i = 4; i <= 1000000; i++) {
-		d[i] = d[i - 1] + 1;
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;
 		pre[i] = i - 1;
-		if (i % 3 == 0) {
-			if (d[i] > (d[i / 3]) + 1) {
-				d[i] = d[i / 3] + 1;
+		if (i % 3 == 0)
+			if (dp[i] > dp[i / 3] + 1) {
+				dp[i] = dp[i / 3] + 1;
 				pre[i] = i / 3;
 			}
-		}
-		if (i % 2 == 0) {
-			if (d[i] > (d[i / 2]) + 1) {
-				d[i] = d[i / 2] + 1;
+
+		if (i % 2 == 0)
+			if (dp[i] > dp[i / 2] + 1) {
+				dp[i] = min(dp[i], dp[i / 2] + 1);
 				pre[i] = i / 2;
 			}
-		}
 	}
 
-	cout << d[n] << "\n";
+	cout << dp[n] << '\n';
 
-	while (n != 0) {
-		cout << n << " ";
-		n = pre[n];
+	int cur = n;
+
+	while(cur){
+		cout << cur << ' ';
+		cur = pre[cur];
 	}
+
 }
