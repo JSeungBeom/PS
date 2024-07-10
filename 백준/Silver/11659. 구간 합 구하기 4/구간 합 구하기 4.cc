a852d5fd4b8533f5f24ad37d4224da72823a85b3
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-10 19:30:35 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -2,29 +2,30 @@
 
 using namespace std;
 
-int n, m, i, j;
-
-int d[100002];
-int arr[100002];
+int n, m;
+int arr[100005];
+int psum[100005];
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n >> m;
-
+	
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
 
-	d[1] = arr[1];
-
-	for (int i = 2; i <= n; i++) {
-		d[i] = d[i - 1] + arr[i];
+	for (int i = 1; i <= n; i++) {
+		psum[i] = psum[i - 1] + arr[i];
 	}
 
+	
 	while (m--) {
+		int i, j;
+
 		cin >> i >> j;
-		cout << d[j] - d[i - 1] << "\n";
+
+		cout << psum[j] - psum[i - 1] << '\n';
 	}
 }
