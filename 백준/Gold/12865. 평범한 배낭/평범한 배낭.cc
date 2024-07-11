2cd07cc4fe57ea8f89785e64fdaf30a9c265752b
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-11 18:08:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 43040 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -2,26 +2,27 @@
 
 using namespace std;
 
-int n, k, w, v;
-int W[103];
-int V[103];
-int dp[103][100003];
+int n, k;
+int W[105];
+int V[105];
+int dp[105][100005];
 
-int main() {
+int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> n >> k;
 
-	
 	for (int i = 1; i <= n; i++) {
 		cin >> W[i] >> V[i];
 	}
 
-	for (int i = 1; i <= n; i++){
+	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= k; j++) {
-			if (j - W[i] >= 0)dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - W[i]] + V[i]);
-			else dp[i][j] = dp[i - 1][j];
+			if (j - W[i] >= 0) dp[i][j] = max(dp[i - 1][j],
+				dp[i - 1][j - W[i]] + V[i]);
+			else
+				dp[i][j] = dp[i - 1][j];
 		}
 	}
 
