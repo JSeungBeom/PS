66445fed5f302fb096af0088f3a023a1dc473ff8
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-11 17:59:58 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -3,9 +3,8 @@
 using namespace std;
 
 int n;
-
-int a[52];
-int b[52];
+int A[105];
+int B[105];
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -14,21 +13,20 @@ int main(void) {
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >> A[i];
 	}
 
 	for (int i = 0; i < n; i++) {
-		cin >> b[i];
+		cin >> B[i];
 	}
 
-	sort(a, a + n);
-	sort(b, b + n);
-	reverse(b, b + n);
+	sort(A, A + n);
+	sort(B, B + n);
 
 	int ans = 0;
 
 	for (int i = 0; i < n; i++) {
-		ans += a[i] * b[i];
+		ans += (A[i] * B[n - i - 1]);
 	}
 
 	cout << ans;
