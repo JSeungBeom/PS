43553799b50320288050c3a53fb00c7f01c93cc0
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-12 18:57:04 +0900

    [Silver IV] Title: GCD 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.cc
+++ 백준/Silver/9613. GCD 합/GCD 합.cc
@@ -1,36 +1,36 @@
 #include <bits/stdc++.h>
+#include <numeric>
 
 using namespace std;
 
-int t, n;
-int num;
+int t;
+int n;
+int arr[105];
 
 int gcd(int a, int b) {
 	if (a == 0) return b;
 	return gcd(b % a, a);
 }
 
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	cin >> t;
 
+	cin >> t;
 	while (t--) {
-		vector<int> arr;
 		cin >> n;
 		long long ans = 0;
-		while (n--) {
-			cin >> num;
-			arr.push_back(num);
+		for (int i = 0; i < n; i++) {
+			cin >> arr[i];
 		}
 
-		for (int i = 0; i < arr.size(); i++) {
-			for (int j = i + 1; j < arr.size(); j++) 
-				ans += gcd(arr[i], arr[j]);	
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				ans += gcd(arr[i], arr[j]);
+			}
 		}
-		
-		cout << ans << "\n";
-		
+
+		cout << ans << '\n';
 	}
 }
