94deb8b85412f67a359af1d4f73b12934a5abc22
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-15 16:40:38 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,25 +1,46 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m, num;
-int a[100003];
+int n, m;
+int arr[100005];
+int num;
+
+int binary_search(int target) {
+	int st = 0;
+	int en = n - 1;
+
+	while (st <= en) {
+		int mid = (st + en) / 2;
+		if (arr[mid] < target)
+			st = mid + 1;
+		else if (arr[mid] > target)
+			en = mid - 1;
+		else
+			return 1;
+	}
+
+	return 0;
+}
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    cin >> n;
+	cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
 
-    sort(a, a + n);
+	sort(arr, arr + n);
 
-    cin >> m;
+	cin >> m;
 
-    for (int i = 0; i < m; i++) {
-        cin >> num;
-        cout << binary_search(a, a + n, num) << "\n";
-    }
+
+	while (m--) {
+		cin >> num;
+
+		cout << binary_search(num) << '\n';
+	}
 }
