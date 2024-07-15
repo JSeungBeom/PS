a89670cd3bbe2d4eaae88db5f88987ae9ed61ccb
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-15 18:15:47 +0900

    [Gold IV] Title: 세 수의 합, Time: 28 ms, Memory: 8304 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -1,40 +1,38 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+using namespace std;
 using ll = long long;
 
 int n;
-int U[1003];
+ll arr[1005];
 vector<ll> two;
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    for (int i = 0; i < n; i++) {
-        cin >> U[i];
-    }
+	cin >> n;
 
-    sort(U, U + n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            two.push_back(U[i] + U[j]);
-        }
-    }
+	sort(arr, arr + n);
 
-    sort(two.begin(), two.end());
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			two.push_back(arr[i] + arr[j]);
+		}
+	}
+	
+	sort(two.begin(), two.end());
 
-    for (int i = n - 1; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
-            if (binary_search(two.begin(), two.end(), U[i] - U[j])) {
-                cout << U[i];
-                return 0;
-            }
-        }
-    }
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (binary_search(two.begin(), two.end(), arr[i] - arr[j])) {
+				cout << arr[i];
+				return 0;
+			}
+		}
+	}
 }
-
-// two[i] + U[k] = U[l] -> U[l] - U[k] = two[i];
