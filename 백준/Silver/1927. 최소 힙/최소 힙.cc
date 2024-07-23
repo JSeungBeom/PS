ffbcbc94863d14b33b3390564bb55387e6730ab9
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-23 17:20:45 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,30 +1,74 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-priority_queue<int, vector<int>, greater<int>> pq;
-int x;
+int heap[100005];
+int sz = 0; // heap에 들어있는 원소의 수
 int n;
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+
+void push(int x) {
+	heap[++sz] = x;
+
+	int idx = sz;
+
+	while (idx != 1) {
+		int par = idx / 2;
+
+		if (heap[par] > heap[idx]) {
+			swap(heap[par], heap[idx]);
+			idx /= 2;
+		}
+		else {
+			return;
+		}
+	}
+}
+
+int top() {
+	if (sz == 0) {
+		return 0;
+	}
+
+	return heap[1];
+}
+
+void pop() {
+	if (sz == 0)
+		return;
+
+	heap[1] = heap[sz--];
+
+	int idx = 1;
 	
+	while (idx * 2 <= sz) {
+		int lc = idx * 2;
+		int rc = idx * 2 + 1;
+		int min_c = lc;
+
+		if (rc <= sz && heap[rc] < heap[lc])
+			min_c = rc;
+		if (heap[idx] <= heap[min_c]) break;
+		swap(heap[idx], heap[min_c]);
+		idx = min_c;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); 
+	cin.tie(0);
+
 	cin >> n;
+
+	int x;
 
 	while (n--) {
 		cin >> x;
-		if (x != 0) {
-			pq.push(x);
+
+		if (x == 0) {
+			cout << top() << '\n';
+			pop();
 		}
 		else {
-			if (pq.empty()) {
-				cout << 0 << '\n';
-			}
-			else {
-				cout << pq.top() << '\n';
-				pq.pop();
-			}
+			push(x);
 		}
 	}
-
 }
