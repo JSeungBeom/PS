3545096dabf23b0550e2e92e85dd665a63950ab8
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-07-30 20:03:57 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -3,12 +3,8 @@
 using namespace std;
 
 int n, m;
-vector<int> adj[1003];
-int indegree[1003];
-queue<int> q;
-vector<int> ans;
-
-
+vector<int> adj[1005];
+int deg[1005];
 
 int main(void) {
 	ios::sync_with_stdio(0);
@@ -16,41 +12,47 @@ int main(void) {
 
 	cin >> n >> m;
 
-	int num, u, v;
+	int num;
+
 	while (m--) {
-		vector<int> tmp;
 		cin >> num;
-		for (int i = 0; i < num; i++) {
+
+		int u;
+		vector<int> tmp;
+
+		while (num--) {
 			cin >> u;
 			tmp.push_back(u);
 		}
 
-		for (int i = 1; i < tmp.size(); i++) {
-			adj[tmp[i - 1]].push_back(tmp[i]);
-			indegree[tmp[i]]++;
+		for (int i = 0; i < (int)tmp.size() - 1; i++) {
+			adj[tmp[i]].push_back(tmp[i + 1]);
+			deg[tmp[i + 1]]++;
 		}
 	}
+	
+	queue<int> q;
 
 	for (int i = 1; i <= n; i++) {
-		if (indegree[i] == 0)
-			q.push(i);
+		if (deg[i] == 0) q.push(i);
 	}
+
+	vector<int> ans;
 
 	while (!q.empty()) {
-		auto cur = q.front(); q.pop();
+		int cur = q.front(); q.pop();
 		ans.push_back(cur);
-		for (auto nxt : adj[cur]) {
-			indegree[nxt]--;
 
-			if (indegree[nxt] == 0)
-				q.push(nxt);
+		for (int nxt : adj[cur]) {
+			deg[nxt]--;
+
+			if (deg[nxt] == 0) q.push(nxt);
 		}
 	}
 
-	if (ans.size() != n) cout << 0;
-	else {
-		for (int e : ans) {
+	if ((int)ans.size() != n)
+		cout << 0;
+	else
+		for (auto e : ans)
 			cout << e << '\n';
-		}
-	}
 }
