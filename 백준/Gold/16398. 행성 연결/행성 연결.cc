4ba6689ec100b9e8c69adb2f26cb1a6a8238e624
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-08-01 19:46:33 +0900

    [Gold IV] Title: 행성 연결, Time: 168 ms, Memory: 19524 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.cc
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -4,51 +4,52 @@
 
 using namespace std;
 
-int n;
-priority_queue<
-	tuple<int, int, int>, 
-	vector<tuple<int, int, int>>, 
-	greater<tuple<int, int, int>>> pq;
-
-vector<pair<int, int>> adj[1000003];
-bool vis[1000003];
-
+int v, e;
 int cost;
-
+priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>,
+	greater<tuple<int, int, int>>> pq;
+vector<pair<int, int>> adj[1005];
+bool vis[1005];
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n;
+	cin >> v;
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
+
+	for (int i = 1; i <= v; i++) {
+		for (int j = 1; j <= v; j++) {
 			cin >> cost;
+
 			if (i >= j) continue;
+
 			adj[i].push_back({ cost, j });
 			adj[j].push_back({ cost, i });
 		}
 	}
-	
+
 	vis[1] = 1;
-	
-	for (auto nxt : adj[1])
+
+	for (auto nxt : adj[1]) {
 		pq.push({ nxt.X, 1, nxt.Y });
+	}
 
 	int cnt = 0;
 	long long ans = 0;
 
-	while (cnt < n - 1) {
-		int weight, a, b;
-		tie(weight, a, b) = pq.top(); pq.pop();
+	while (cnt < v - 1) {
+		int a, b, c;
+		tie(c, a, b) = pq.top(); pq.pop();
 		if (vis[b]) continue;
-		ans += weight;
-		cnt++;
 		vis[b] = 1;
+		ans += c;
+		cnt++;
+
 		for (auto nxt : adj[b]) {
-			if (!vis[nxt.Y])
-				pq.push({ nxt.X, b, nxt.Y });
+			if (vis[nxt.Y]) continue;
+
+			pq.push({ nxt.X, b, nxt.Y });
 		}
 	}
 
