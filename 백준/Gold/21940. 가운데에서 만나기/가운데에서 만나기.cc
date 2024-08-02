4eab0d4ee176924bfedbece82f8dccc982092767
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-08-02 20:13:14 +0900

    [Gold IV] Title: 가운데에서 만나기, Time: 16 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.cc
+++ 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.cc
@@ -1,68 +1,74 @@
 #include <bits/stdc++.h>
+#define X first
+#define Y second
+
 #define INF 0x3f3f3f3f
+
 using namespace std;
 
-int n, m;
-int a, b, t;
-int k, c;
-int d[203][203];
-int friends[203];
-vector<int> city;
-set<int> s;
+int n, m, k;
+int dist[205][205];
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+
+	int a, b, c;
+
 	cin >> n >> m;
 
 	for (int i = 1; i <= n; i++) {
-		fill(d[i], d[i] + n + 1, INF);
-	}
-	while (m--) {
-		cin >> a >> b >> t;
-		d[a][b] = t;
+		fill(dist[i], dist[i] + n + 1, INF);
 	}
 
-	cin >> k;
-	for (int i = 1; i <= k; i++) {
-		cin >> c;
-		friends[i] = c;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> c;
+
+		dist[a][b] = min(dist[a][b], c);
 	}
 
 	for (int i = 1; i <= n; i++) {
-		d[i][i] = 0;
+		dist[i][i] = 0;
 	}
 
 	for (int mid = 1; mid <= n; mid++) {
 		for (int st = 1; st <= n; st++) {
 			for (int en = 1; en <= n; en++) {
-				d[st][en] = min(d[st][en], d[st][mid] + d[mid][en]);
+				dist[st][en] = min(dist[st][en],
+					dist[st][mid] + dist[mid][en]);
 			}
 		}
 	}
 
-	int ans = 0x7f7f7f7f;
+	cin >> k;
+
+	vector<int> friends;
+
+	int town;
+
+	for (int i = 0; i < k; i++) {
+		cin >> town;
+		friends.push_back(town);
+	}
+
+	int mn = 0x7f7f7f7f;
 
 	for (int i = 1; i <= n; i++) {
 		int mx = 0;
-		for (int j = 1; j <= k; j++) {
-			mx = max(mx, d[friends[j]][i] + d[i][friends[j]]);
+		for (auto city : friends) {
+			mx = max(mx, dist[city][i] + dist[i][city]);
 		}
-		ans = min(mx, ans);
+		mn = min(mx, mn);
 	}
 
 	for (int i = 1; i <= n; i++) {
 		int mx = 0;
-		for (int j = 1; j <= k; j++) {
-			mx = max(mx, d[friends[j]][i] + d[i][friends[j]]);
+		for (auto city : friends) {
+			mx = max(mx, dist[city][i] + dist[i][city]);
 		}
-		if (mx == ans) s.insert(i);
+		if (mx == mn)
+			cout << i << ' ';
 	}
 
-	vector<int> v(s.begin(), s.end());
-	sort(v.begin(), v.end());
-	for (auto e : v) {
-		cout << e << ' ';
-	}
+
 }
