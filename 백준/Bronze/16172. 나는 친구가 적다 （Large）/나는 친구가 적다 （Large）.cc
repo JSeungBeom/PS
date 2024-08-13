5f91f486cde4f21611a16782385042137e823993
JSeungBeom <99180728+JSeungBeom@users.noreply.github.com>
2024-08-13 18:37:52 +0900

    [Bronze II] Title: 나는 친구가 적다 (Large), Time: 4 ms, Memory: 3700 KB -BaekjoonHub

---
+++ 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> failure(string& s) {
+	vector<int> f(s.size());
+	int j = 0;
+	for (int i = 1; i < s.size(); i++) {
+		while (j > 0 && s[i] != s[j]) j = f[j - 1];
+		if (s[i] == s[j]) f[i] = ++j;
+	}
+
+	return f;
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s, p;
+
+	cin >> s >> p;
+
+	vector<int> f = failure(p);
+
+	string tmp = "";
+
+	for (int i = 0; i < s.length(); i++) {
+		if (0 <= s[i] - '0' && s[i] - '0' <= 9)
+			continue;
+
+		tmp.push_back(s[i]);
+	}
+
+	int j = 0;
+
+	for (int i = 0; i < tmp.size(); i++) {
+		while (j > 0 && tmp[i] != p[j]) j = f[j - 1];
+		if (tmp[i] == p[j]) j++;
+		if (j == p.size()) {
+			cout << 1;
+			return 0;
+		}
+	}
+	
+	cout << 0;
+}
