92f294e5df3cbce340dec5f5396462948d341452
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2024-09-27 16:43:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    
+    while(a != b) {
+        if(a % 2 == 0)
+            a /= 2;
+        else
+            a = a / 2 + 1;
+        
+        if(b % 2 == 0)
+            b /= 2;
+        else
+            b = b / 2 + 1;
+        
+        answer++;
+    }
+    
+    
+    return answer;
+}
