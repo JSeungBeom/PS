e516f9c73fdc3f4b253444cee6159096479ab7d8
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2024-11-01 02:44:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 524 ms, Memory: 39028 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N, M, V;
+
+    static LinkedList<Integer>[] adjList;
+    static boolean[] dfs_vis;
+
+    static void BFS(int st){
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[N + 1];
+
+        q.add(st);
+        vis[st] = true;
+
+        while(!q.isEmpty()){
+            int cur = q.remove();
+
+            System.out.print(cur + " ");
+            for(int nxt : adjList[cur]){
+                if(!vis[nxt]){
+                    vis[nxt] = true;
+                    q.add(nxt);
+                }
+            }
+        }
+    }
+
+    static void DFS(int cur){
+        if(dfs_vis[cur]) return;
+
+        dfs_vis[cur] = true;
+        System.out.print(cur + " ");
+        for(int nxt : adjList[cur]){
+            DFS(nxt);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        V = sc.nextInt();
+
+        adjList = new LinkedList[N + 1];
+        dfs_vis = new boolean[N + 1];
+
+        for(int i = 1; i <= N; i++){
+            adjList[i] = new LinkedList<>();
+        }
+
+        for(int i = 0; i < M; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
+
+        for(int i = 1; i <= N; i++){
+            Collections.sort(adjList[i]);
+        }
+
+        DFS(V);
+        System.out.println();
+        BFS(V);
+    }
+
+}
