2b4c123f54f5f6bb64f18cf56d01bb93d027d09d
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2024-11-08 23:45:28 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 432 ms, Memory: 52204 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -1,73 +1,72 @@
-#include <bits/stdc++.h>
- 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <tuple>
+#define X first
+#define Y second
+
 using namespace std;
- 
-struct P{
-    int r, c;       // 현재 위치
-    int cnt = 1;    // 지금까지 지나온 칸 수
-    int wall = 0;   // 지금까지 부순 벽의 수 
-};
- 
-int N, M, K;
-int Map[1001][1001];
-int dr[4] = {-1, 1, 0, 0}, dc[4] = {0, 0, -1, 1};
-int visited[1001][1001][11] = {0, };
- 
-void input(){
-    cin >> N >> M >> K;
-    string in;
-    for(int i = 0; i < N; i++){
-        cin >> in;
-        for(int j = 0; j < M; j++){
-            Map[i][j] = in[j] - '0';
-        }
-    }
+
+int n, m, k;
+queue<tuple<int, int, int>> q;
+int dist[1005][1005][12];
+bool board[1005][1005];
+
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			fill(dist[i][j], dist[i][j] + k + 1, -1);
+
+	string s;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++) {
+			board[i][j] = s[j] - '0';
+		}
+	}
+
+	dist[0][0][0] = 1;
+	q.push({ 0, 0, 0 });
+
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+
+		int x, y, broke;
+		tie(x, y, broke) = cur;
+
+		if (x == n - 1 && y == m - 1) {
+			cout << dist[x][y][broke] << '\n';
+			return 0;
+		}
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+			if (board[nx][ny] == 0 && dist[nx][ny][broke] < 0) {
+				dist[nx][ny][broke] = dist[x][y][broke] + 1;
+				q.push({ nx, ny, broke });
+			}
+			else if (broke < k && board[nx][ny] == 1 && dist[nx][ny][broke + 1] < 0) {
+				dist[nx][ny][broke + 1] = dist[x][y][broke] + 1;
+				q.push({ nx, ny, broke + 1 });	
+			}
+
+		}
+	}
+
+	cout << -1;
 }
- 
-int BFS(){
-    int result = 0;
-    queue<P> que;
-    P temp;
-    temp.r = 0;  temp.c = 0;
-    que.push(temp);
- 
-    visited[0][0][0] = 1;
- 
-    while(!que.empty()){
-        P now = que.front();
-        que.pop();
-        
-        // BFS 이므로 가장 먼저 도착한 경우임
-        if(now.r == N-1 && now.c == M-1) return now.cnt;
-        
-        for(int i = 0; i < 4; i++){
-            int nr = now.r + dr[i];
-            int nc = now.c + dc[i];
-            
-            // 좌표를 넘어가는 경우
-            if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-            // 이미 방문한 경우
-            if(visited[nr][nc][now.wall]) continue;
-            
-            // 다음이 벽이 아니고, 방문하지 않았다면
-            if(!Map[nr][nc] && !visited[nr][nc][now.wall]){
-                visited[nr][nc][now.wall] = 1;
-                que.push({nr, nc, now.cnt+1, now.wall});
-            }
-            // 다음이 벽이고, 벽을 최소 하나 더 부술 수 있고, 하나 더 부순 경우에 방문하지 않았다면
-            if(Map[nr][nc] && now.wall < K && !visited[nr][nc][now.wall+1]){
-                visited[nr][nc][now.wall+1] = 1;
-                que.push({nr, nc, now.cnt+1, now.wall+1});
-            }
-        }
-    }
-    
-    return -1;
-}
- 
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    input();
-    cout << BFS();
-    return 0;
-}
+
