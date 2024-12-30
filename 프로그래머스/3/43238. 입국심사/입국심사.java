70f17d074425583757922a156515ab645a652f0d
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2024-12-30 17:54:47 +0900

    [level 3] Title: 입국심사, Time: 69.84 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public boolean binary_search(int n, int[] times, long mid){
+        long cnt = 0L;
+        
+        for(int time : times){
+            cnt += (mid / time);
+        }
+        
+        return cnt >= n;
+    }
+    
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        long st = 0;
+        long en = 1000000000 * 1000000000L;
+                
+        while(st < en){
+            long mid = (st + en) / 2;
+            
+            if(binary_search(n, times, mid))
+                en = mid;
+            else
+                st = mid + 1;
+        }
+                
+        return st;
+    }
+}
