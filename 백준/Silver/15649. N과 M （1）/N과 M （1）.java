c8d0eebb0c31cb87e0c62e489d98275f0a7febe2
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-10 09:01:50 +0900

    [Silver III] Title: N과 M (1), Time: 1592 ms, Memory: 51140 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,46 +1,45 @@
-import java.util.*;
-import java.io.*;
+// nCr 공식을 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static boolean[] vis;
-    static int[] arr;
-    static int N;
-    static int M;
-
-    static void back_tracking(int n, int k){
-        if(n == k){
-            for(int i = 0; i < n; i++){
-                System.out.print(arr[i] + " ");
-            }
-
-            System.out.println();
-
-            return;
-        }
-
-        for(int i = 1; i <= N; i++){
-            if(vis[i]) continue;
-
-            vis[i] = true;
-            arr[n] = i;
-            back_tracking(n + 1, k);
-            vis[i] = false;
-        }
+	
+		static boolean[] vis = new boolean[10];
+		static int[] tgt = new int[10];
+		static int N, M;
+		
+		public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	
+		perm(0);
+		
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        vis = new boolean[N + 1];
-        arr = new int[N + 1];
-
-        back_tracking(0, M);
-
-    }
+		
+		static void perm(int idx) {
+			if(idx == M) {
+				for (int i = 0; i < M; i++) {
+					System.out.print(tgt[i] + " ");
+				}
+				System.out.println();
+				
+				return;
+			}
+			
+			for(int i = 1; i <= N; i++) {
+				if(vis[i]) continue;
+				
+				tgt[idx] = i;
+				vis[i] = true;
+				perm(idx + 1);
+				vis[i] = false;
+			}
+		}
 }
