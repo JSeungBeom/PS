8830a8b16da9eb76ca22bbf4728ddb148ede50ac
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-10 13:36:46 +0900

    [level 2] Title: 올바른 괄호, Time: 12.06 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 54.3 MB, 시간: 13.91 ms
+메모리: 54.8 MB, 시간: 12.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 10일 11:32:28
+2025년 02월 10일 13:36:42
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        
-        Deque<Character> stack = new ArrayDeque<>();
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push(s.charAt(i));
-            } else{
-                if(stack.isEmpty()) return false;
-                
-                stack.pop();
-            }
-        }
-               
-        if(stack.isEmpty()) return true;
-        
-        return false;
-    }
+	boolean solution(String s) {
+		char[] input = s.toCharArray();
+		int len = input.length;
+		int xCnt = 0; // ()가 한쌍으로 일치하면 'X'로 표시, 그 건수
+		for (int i = 0, oi = 0; i < len; i++) {
+			if(input[i] == ')') { // 닫는 괄호
+				if( input[oi] != '(' ) return false;
+				
+				// 두 괄호가 일치
+				input[i] = 'X';
+				input[oi] = 'X';
+				xCnt += 2; // 일치해서 처리된 항목 수 2 증가 
+				
+				while(oi > 0 && input[oi] == 'X') oi--; // 현재 oi 자리가 'X'가 되면서 이전 자리 중 여는 괄호가 있는 마지막 위치
+			} else { // 여는 괄호
+				oi = i;
+			}
+		}
+		
+		return xCnt == len;
+	}
 }
