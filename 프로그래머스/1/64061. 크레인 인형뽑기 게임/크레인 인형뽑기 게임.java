004842c735d5bfcad114ebd5a17e8f6a8c731fc5
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-10 15:29:02 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 3.03 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,36 +1,43 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        
-        Deque<Integer> stack = new ArrayDeque<>();
-        
-        for(int move : moves){
-            int x = move - 1;
-            int y = 0;
-            
-            while(y < board.length && board[y][x] == 0) y++;
-            
-            if(y >= board.length) continue;
-            
-            if(stack.isEmpty()){
-                stack.push(board[y][x]);
-                board[y][x] = 0;
-            } else {
-                if(stack.peek() == board[y][x]){
-                    stack.pop(); answer += 2;
-                    board[y][x] = 0;
-                }
-                else {
-                    stack.push(board[y][x]);
-                    board[y][x] = 0;
-                }     
-            }
-            
-           
-        }
-        
-        return answer;
+public int solution(int[][] board, int[] moves) {
+    	Stack<Integer>[] lanes = new Stack[board.length];
+    	for (int i = 0; i < lanes.length; i++) {
+			lanes[i] = new Stack<>();
+		}
+    	
+    	// 그리드 형태의 각 열별 Stack에 입력을 담는다.
+    	
+    	for (int i = board.length - 1; i >= 0; i--) {
+			for(int j = 0; j < board[i].length; j++) {
+				if(board[i][j] > 0) {
+					lanes[j].push(board[i][j]);
+				}
+			}
+		}
+    	
+    	// 스택의 내용 확인
+    	for (int i = 0; i < lanes.length; i++) {
+			System.out.println(Arrays.toString(lanes[i].toArray())); // Array로 변경하여 출력
+		}
+    	
+    	Stack<Integer> bucket = new Stack<>();
+    	int answer = 0;
+    	
+    	for(int move : moves) { // 1 based index
+     		if(lanes[move - 1].isEmpty()) continue;
+     		
+     		int doll = lanes[move - 1].pop();
+     		
+     		if(!bucket.isEmpty() && doll == bucket.peek()) { // 같은 읺여
+     			bucket.pop();
+     			answer += 2;
+     		} else {
+     			bucket.push(doll);
+     		}
+    	}
+    
+    	 return answer;
     }
 }
