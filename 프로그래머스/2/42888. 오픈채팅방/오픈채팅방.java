9a8569025fabea8681ca98a359a5ebce1c64f689
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-11 13:49:22 +0900

    [level 2] Title: 오픈채팅방, Time: 205.81 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,46 +1,36 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] record) {
-        HashMap<String, String> hashMap = new HashMap<>();
-        
-        int length = 0;
-        
-        for(int i = 0; i < record.length; i++){
-            StringTokenizer st = new StringTokenizer(record[i]);
-            
-            String com = st.nextToken();
-            String uid = st.nextToken();
-            if(com.equals("Enter") || com.equals("Change")){
-                String nickname = st.nextToken();
-                hashMap.put(uid, nickname);    
-            } 
-            
-            if(com.equals("Enter") || com.equals("Leave")){
-                length++;
-            }
-        }
-        
-        String[] answer = new String[length];
-        int idx = 0;
-        
-        for(int i = 0; i < record.length; i++){
-            StringTokenizer st = new StringTokenizer(record[i]);
-            
-            String com = st.nextToken();
-            String uid = st.nextToken();
-            
-            if(com.equals("Enter")){
-                String nickname = hashMap.get(uid);
-                
-                answer[idx++] = nickname + "님이 들어왔습니다.";
-            } else if(com.equals("Leave")){
-                String nickname = hashMap.get(uid);
-                
-                answer[idx++] = nickname + "님이 나갔습니다.";
-            }
-        }
-        
-        return answer;
-    }
+   public String[] solution(String[] record) {
+		// Enter, Leave에 대한 문자열로 HashMap에 관리
+		HashMap<String, String> msg = new HashMap<>();
+		msg.put("Enter", "님이 들어왔습니다.");
+		msg.put("Leave", "님이 나갔습니다.");
+		
+		// 유저 아이디별 닉네임 관리 HashMap
+		HashMap<String, String> uid = new HashMap<>();
+		
+		for (String s : record) {
+			String[] cmd = s.split(" ");
+			
+			if(cmd.length == 3) {
+				uid.put(cmd[1], cmd[2]); // cmd[1] : 유저 아이디인데, 이 값이 중복되면 덮어쓴다
+			}
+		}
+		
+		// 정답 구성
+		ArrayList<String> answer = new ArrayList<>();
+		
+		for (String s : record) {
+			String[] cmd = s.split(" ");
+			
+			// Enter, Leave에 대해서만 처리 -> msg 이용
+			if(msg.containsKey(cmd[0])) { // Change는 거른다
+				answer.add(uid.get(cmd[1]) + msg.get(cmd[0]));
+			}
+		}
+		
+		return answer.toArray(new String[0]); // ArrayList => String[]
+		
+	}
 }
