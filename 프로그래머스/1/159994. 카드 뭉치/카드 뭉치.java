d9e7b85ec00aea4de075192e32a6f9f67e325670
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-11 10:47:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.67 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,38 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        
-        Queue<String> queue1 = new ArrayDeque<>();
-        Queue<String> queue2 = new ArrayDeque<>();
-        
-        for(String card : cards1){
-            queue1.offer(card);
-        }
-        
-        for(String card : cards2){
-            queue2.offer(card);
-        }
-        
-        int len = 0;
-        
-        for(int i = 0; i < goal.length; i++){
-
-            if(!queue1.isEmpty() && queue1.peek().equals(goal[i])){
-                queue1.poll(); len++;
-            }
-            else if(!queue2.isEmpty() && queue2.peek().equals(goal[i])){
-                queue2.poll(); len++;
-            }
-        }
-        
-        System.out.println(len);
-        
-        if(len == goal.length)
-            answer = "Yes";
-        else
-            answer = "No";
-        
-        return answer;
-    }
+		Queue<String> cardsDeque1 = new ArrayDeque<>(Arrays.asList(cards1));
+		Queue<String> cardsDeque2 = new ArrayDeque<>(Arrays.asList(cards2));
+		Queue<String> goalDeque = new ArrayDeque<>(Arrays.asList(goal));
+		
+		while(!goalDeque.isEmpty()) {
+			if(!cardsDeque1.isEmpty() && cardsDeque1.peek().equals(goalDeque.peek())) {
+				cardsDeque1.poll();
+				goalDeque.poll();
+			} else if(!cardsDeque2.isEmpty() && cardsDeque2.peek().equals(goalDeque.peek())) {
+				cardsDeque2.poll();
+				goalDeque.poll();
+			}
+			else {
+				break;
+			}
+		}
+		
+		if(goalDeque.isEmpty()) return "Yes";
+		else return "No";
+		
+	}
 }
