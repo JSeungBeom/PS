28f8d0a01710130aa958bd5945391f698143c44c
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-12 15:11:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,31 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int N;
 
-        int N = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		
+		while(true) {
+			if(N < 0) {
+				System.out.println(-1); // 3과 5로 만들지 못하는 경우
+				break;
+			}
+			
+			// N을 5로 나누어 떨어지는지
+			if(N % 5 == 0) {
+				System.out.println(answer + N / 5);
+				break;
+			}
+			
+			N -= 3;
+			answer++;
+		}
+	}
 
-        int[] dp = new int[5005];
-
-        for(int i = 1; i <= 5000; i++)
-            dp[i] = -1;
-
-        dp[0] = 0; dp[3] = 1;
-
-        for(int i = 5; i <= N; i++){
-            dp[i] = Integer.MAX_VALUE;
-
-            if(dp[i - 5] >= 0)
-                dp[i] = Math.min(dp[i - 5] + 1, dp[i]);
-            if(dp[i - 3] >= 0)
-                dp[i] = Math.min(dp[i - 3] + 1, dp[i]);
-
-            if(dp[i] == 0x7fffffff)
-                dp[i] = -1;
-        }
-
-        System.out.print(dp[N]);
-    }
 }
