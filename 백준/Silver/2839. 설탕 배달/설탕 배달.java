6a68c387a68acf6115a03d375b4af1dcd48c8af3
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-12 15:01:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -15,7 +15,7 @@ public class Main {
         dp[0] = 0; dp[3] = 1;
 
         for(int i = 5; i <= N; i++){
-            dp[i] = 0x7fffffff;
+            dp[i] = Integer.MAX_VALUE;
 
             if(dp[i - 5] >= 0)
                 dp[i] = Math.min(dp[i - 5] + 1, dp[i]);
