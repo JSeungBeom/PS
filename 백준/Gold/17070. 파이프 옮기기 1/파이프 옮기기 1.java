728553b843e5144424222632ec5248da6e1b8f7a
hmmmman <99180728+JSeungBeom@users.noreply.github.com>
2025-02-21 14:21:07 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -5,9 +5,8 @@ public class Main {
 	
 	static int N;
 	static int[][] board;
-	static boolean[][] vis;
-	static int[] dx = { 0, 1, 1 };
-	static int[] dy = { 1, 0, 1 };
+	static int[][][] dp;
+	
 	static int ans;
 	
 	public static void main(String[] args) throws Exception {
@@ -16,7 +15,7 @@ public class Main {
     	N = Integer.parseInt(br.readLine());
     	
     	board = new int[N + 1][N + 1];
-    	vis = new boolean[N + 1][N + 1];
+    	dp = new int[N + 1][N + 1][3];
     	
     	for(int i = 1; i <= N; i++) {
     		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,42 +24,23 @@ public class Main {
     		}
     	}
     	
-    	dfs(1, 2, 0);
-    	System.out.println(ans);
+    	dp[1][2][0] = 1;
+    	for(int i = 1; i <= N; i++) {
+    		for(int j = 1; j <= N; j++) {
+    			if(board[i][j] != 1) {
+
+    				dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
+    				dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
+    			}
+    			if(board[i][j] != 1 && board[i - 1][j] != 1 && board[i][j - 1] != 1) {
+    				dp[i][j][2] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+    			}
+    		}
+    	}
+    	
+
+    	System.out.println(dp[N][N][0] + dp[N][N][1] + dp[N][N][2]);
 	}	
-	
-	static void dfs(int x, int y, int dir) {
-		if(x == N && y == N) {
-			ans++;
-			return;
-		}
-		
-		vis[x][y] = true;
-		
-		
-		for(int d = 0; d < 3; d++) {
-			if(dir == 0 && d == 1) continue;
-			if(dir == 1 && d == 0) continue;
-			
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			
-			if(nx < 0 || nx > N || ny < 0 || ny > N) continue;
-			
-			if(d < 2) {
-				if(board[nx][ny] == 0 && !vis[nx][ny]) {
-					dfs(nx, ny, d);
-					vis[nx][ny] = false;
-				}
-			}
-			else {
-				if(board[nx][ny] == 0 && board[nx - 1][ny] == 0 &&
-						board[nx][ny - 1] == 0 && !vis[nx][ny]) {
-					dfs(nx, ny, d);
-					vis[nx][ny] = false;
-				}
-			}
-		}
-	}
+
 	
 }
